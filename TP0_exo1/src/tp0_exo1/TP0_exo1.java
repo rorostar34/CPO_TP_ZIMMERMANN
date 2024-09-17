b07c0ec2fbd6a5b7c989b3ce44c8da040f0a37d8
33669 <33669@10.3.62.153>
2024-09-17 16:45:33 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_exo1/src/tp0_exo1/TP0_exo1.java
@@ -0,0 +1,30 @@
+/*
+ * TP0 Exercice 1
+ * TDC
+ * 17/09/2024
+ */
+package tp0_exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rorostar34
+ */
+public class TP0_exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        System.out.println("au revoir");
+        String prenom;
+        Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+prenom = sc.nextLine();
+    }
+    
+}
