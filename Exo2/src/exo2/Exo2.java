8048fe90d2eef35eeeb3580d951762bb3d420fa5
33669 <33669@10.3.62.153>
2024-09-20 12:37:51 +0200

    tp 0 exo 2

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -5,6 +5,8 @@
  */
 package exo2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 33669
@@ -15,7 +17,31 @@ public class Exo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+//Declaration des variables
+        
+        int result;
+        int ind;
+       
+        result = 0;
+        
+        
+        
+        
+        int nb;
+//nb=5;
+Scanner sc = new Scanner(System.in);
+System.out.println("\n Entrer le nombre :");
+nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+
+// Addition des nb premiers entiers
+        ind = 1;
+        while (result <= nb) {
+            result = result + ind;
+        }
+
+// Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des " + nb + "entiers est: " + result);
+
+}
 }
