d8b1667a88ab51345eb68ce4a42ee1ea64172fca
33669 <33669@10.3.62.153>
2024-09-23 11:15:22 +0200

    tp1 partie1

---
+++ TP1_convertisseur_ZIMMERMANN/src/tp1_convertisseur_zimmermann/TP1_convertisseur_ZIMMERMANN.java
@@ -0,0 +1,29 @@
+/*
+ * TP1 partie 2
+ * TDC
+ * 23/09/2024
+ */
+package tp1_convertisseur_zimmermann;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 33669
+ */
+public class TP1_convertisseur_ZIMMERMANN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double val;
+        double conv;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer une valeur réelle:");
+        val=sc.nextDouble();
+    }
+public static double CelsiusVersKelvin (double tCelsius)
+    }
+    
+}
