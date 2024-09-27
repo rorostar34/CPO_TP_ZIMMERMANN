c6e01d6b1a156a61c9df6f231685d0269ca3fda5
33669 <33669@10.3.62.153>
2024-09-27 15:24:26 +0200

    tp1 guess my number

---
+++ TP1_guessMyNumber_ZIMMERMANN/src/tp1_guessmynumber_zimmermann/TP1_guessMyNumber_ZIMMERMANN.java
@@ -0,0 +1,35 @@
+/*
+ * TP1 partie 3
+ * TDC
+ * 27/09/2024
+ */
+
+package tp1_guessmynumber_zimmermann;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 33669
+ */
+public class TP1_guessMyNumber_ZIMMERMANN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Random generateurAleat = new Random();
+    int n = generateurAleat.nextInt(100);
+    int nb;
+    System.out.println(n);
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\n Entrer un nombre entre 0 et 100:");
+    nb = sc.nextInt();
+    
+    
+    
+
+    }
+    
+}
